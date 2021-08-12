4fd18b8effa10853776cb64f953dfc0e47b9f4d7
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-12 12:08:27 -0500

    added Snipcart to guest layout

--- resources/views/livewire/catalog-item.blade.php
+++ resources/views/livewire/catalog-item.blade.php
@@ -7,7 +7,7 @@
         <button class="snipcart-add-item px-4 py-3 bg-indigo-700 text-white"
             data-item-id="stock-the-pantry"
             data-item-price="79.99"
-            data-time-url="https://maryparrish2021.test/catalog/item/{{ $CatalogItem->id }}"
+            data-time-url="/catalog/item/{{ $CatalogItem->id }}"
             data-item-description="High-quality replica of The Starry Night by the Dutch post-impressionist painter Vincent van Gogh."
             data-item-name="{{ $CatalogItem->name }}">
             Add to cart
