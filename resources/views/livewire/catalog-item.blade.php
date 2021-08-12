49f0ea52403dec2f9d6f2f4fc94312d86eb5635c
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-12 16:29:49 -0500

    testing decimals

--- resources/views/livewire/catalog-item.blade.php
+++ resources/views/livewire/catalog-item.blade.php
@@ -3,10 +3,13 @@
     <div class="text-3xl">
         {{ $CatalogItem->name }}
     </div>
+    <div class="mt-4">
+        {{ $CatalogItem->cost}}
+    </div>
     <div class="mt-8">
         <button class="snipcart-add-item px-4 py-3 bg-indigo-700 text-white"
             data-item-id="stock-the-pantry"
-            data-item-price="79.99"
+            data-item-price="{{ $CatalogItem->cost }}"
             data-item-url="/catalog/item/{{ $CatalogItem->id }}"
             data-item-description="High-quality replica of The Starry Night by the Dutch post-impressionist painter Vincent van Gogh."
             data-item-name="{{ $CatalogItem->name }}">
