048ecc1d539f73dda481bc67dd37dfcf659915c7
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-12 21:43:07 -0500

    webhook

--- app/Http/Livewire/WebhookConfirmation.php
+++ app/Http/Livewire/WebhookConfirmation.php
@@ -10,7 +10,7 @@ class WebhookConfirmation extends Component
     public $result;
     public function mount(Request $request)
     {
-        $this->result = $this->request;
+        $this->result = $request;
     }
 
     public function render()
