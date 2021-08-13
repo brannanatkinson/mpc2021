ea3bbfb751d1448e85fda63d24dd20fe1c1d2284
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-12 22:34:10 -0500

    webhook

--- resources/views/livewire/webhook-confirmation.blade.php
+++ resources/views/livewire/webhook-confirmation.blade.php
@@ -3,7 +3,7 @@
         <div class="text-3xl">Webhook Confirmation</div>
     </div>
     <div>
-        @foreach ($results['content']['items'] as $item)
+        @foreach ($result['content']['items'] as $item)
             
         @endforeach
     </div>
