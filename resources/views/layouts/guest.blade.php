0f8500968e3000357e310912609d7fb6ba1309a1
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-12 22:44:19 -0500

    webhook

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -25,7 +25,7 @@
             {{ $slot }}
         </div>
         <script async src="https://cdn.snipcart.com/themes/v3.2.1/default/snipcart.js"></script>
-        <div id="snipcart" data-config-modal-style="side" data-api-key="YzI0NmM1NGUtMTM1Ny00YmQ0LWJkNzAtMGVlM2Q5MzdjNDFhNjM3Mjk5NDMxMDg0MzAwMzk2" hidden></div>
+        <div id="snipcart" data-api-key="YzI0NmM1NGUtMTM1Ny00YmQ0LWJkNzAtMGVlM2Q5MzdjNDFhNjM3Mjk5NDMxMDg0MzAwMzk2" hidden></div>
 
     </body>
 </html>
