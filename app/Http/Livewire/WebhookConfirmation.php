7dc0b7338a38bb1017b8c13d9d4ba3b56bfe57b3
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-12 21:46:25 -0500

    webhook

--- app/Http/Livewire/WebhookConfirmation.php
+++ app/Http/Livewire/WebhookConfirmation.php
@@ -10,7 +10,7 @@ class WebhookConfirmation extends Component
     public $result;
     public function mount(Request $request)
     {
-        $this->result = 'brannan';
+        $this->result = $request;
     }
 
     public function render()
