6ed2bda2427c228095651f9fdafdcff48d62760c
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-12 21:11:44 -0500

    webhook

--- app/Http/Livewire/WebhookConfirmation.php
+++ app/Http/Livewire/WebhookConfirmation.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 class WebhookConfirmation extends Component
 {    
-    public function render()
+    public function render(Request $request)
     {
         return view('livewire.webhook-confirmation');
     }
