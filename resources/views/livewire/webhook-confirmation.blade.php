0ce1437f1cc560ed51e1e97f09a6a3859c5a7690
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-12 22:25:21 -0500

    webhook

--- resources/views/livewire/webhook-confirmation.blade.php
+++ resources/views/livewire/webhook-confirmation.blade.php
@@ -3,6 +3,6 @@
         <div class="text-3xl">Webhook Confirmation</div>
     </div>
     <div>
-        {{ $result['content']['items'] }}
+        {{ $result['content']['items'][0]['name'] }}
     </div>
 </div>
