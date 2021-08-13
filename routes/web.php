0890b248135480b1c22b1cddd4b8e2697fb1a49c
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-12 21:45:05 -0500

    webhook

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
 
 Route::get('/catalog', Catalog::class)->name('catalog');
 Route::get('/catalog/item/{id}', CatalogItem::class);
-Route::get('/webhook', WebhookConfirmation::class);
+Route::post('/webhook', WebhookConfirmation::class);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
