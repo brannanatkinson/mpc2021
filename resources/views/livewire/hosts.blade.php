c95484c435ba1b57e0846fe48c27fb18ac99b082
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-17 12:41:29 -0500

    added donor

--- resources/views/livewire/hosts.blade.php
+++ resources/views/livewire/hosts.blade.php
@@ -2,9 +2,7 @@
     @foreach( $hosts as $host )
     <h2 class="text-3xl">{{ $host->name }}</h2>
     <div class="my-4">
-        @foreach ( $host->sales() as $sale )
-            {{ $sale }}
-        @endforeach
+        $host->sales()
     </div>
     @endforeach
 </div>
