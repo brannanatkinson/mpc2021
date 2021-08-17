0a84b0bdf0c62c2b4c03bb038f143076473156a8
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-16 23:58:11 -0500

    added donor

--- app/Models/Host.php
+++ app/Models/Host.php
@@ -17,13 +17,7 @@ class Host extends Model
 
     public function sales()
     {
-        return DB::table('hosts')
-            ->join('host_item', 'hosts.id', '=', 'host_item.host_id')
-            ->join('items', 'items.id', '=', 'host_item.item_id')
-            ->select('items.name as Item Name', DB::raw('SUM(host_item.item_quantity) as Quantity') )
-            ->groupBy('items.name')
-            ->where('hosts.id', '=', $this->id)
-            ->get();
+        return $this->id;
         
     }
 }
