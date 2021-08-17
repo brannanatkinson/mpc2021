2c2ad1832c558d9e19b222180b4756cf3f0cbfb0
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-17 15:18:41 -0500

    added donor

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -21,7 +21,13 @@ class Item extends Model
 
     public function sales()
     {
-        $sales = DB::table('items')->get();
+        $sales = DB::table('items')
+            ->join('gift_item', 'items.id', '=', 'gift_item.host_id')
+            ->join('gifts', 'items.id', '=', 'gift_item.item_id')
+            ->select('items.name as item_name', DB::raw('SUM(gift_item.item_quantity) as quantity') )
+            ->groupBy('items.name')
+            ->where('items.id', '=', $this->id )
+            ->get();
         return $sales;
     }
 }
--- resources/views/livewire/items.blade.php
+++ resources/views/livewire/items.blade.php
@@ -1,7 +1,7 @@
 <div>
     <h1 class="text-4xl">Items</h1>
     @foreach ( $items as $item )
-        <h2 class="text-3xl">x{{ $item->name }}</h2>
+        <h2 class="text-3xl">{{ $item->name }}</h2>
         {{ $item->sales() }}
     @endforeach
 </div>
