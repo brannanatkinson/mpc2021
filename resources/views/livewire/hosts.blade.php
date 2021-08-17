b84836f6510767086676ef7fc8bf87465d07bbb1
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-17 01:09:23 -0500

    added donor

--- resources/views/livewire/hosts.blade.php
+++ resources/views/livewire/hosts.blade.php
@@ -3,7 +3,7 @@
     <h2 class="text-3xl">{{ $host->name }}</h2>
     <div class="my-4">
         @foreach ( $host->items as $item )
-        {{ $item->name }} : {{ $item->sum('pivot.quantity_ordered') }}
+        {{ $item->name }} : {{ $item->sum('pivot.item_quantity') }}
         @endforeach
     </div>
     @endforeach
