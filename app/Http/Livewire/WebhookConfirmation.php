feb162fcef7a0e94e0bc4d05aee059b35ab61e88
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-16 21:57:06 -0500

    added donor

--- app/Http/Livewire/WebhookConfirmation.php
+++ app/Http/Livewire/WebhookConfirmation.php
@@ -37,7 +37,7 @@ class WebhookConfirmation extends Component
         foreach ( $this->result['content']['items'] as $newItem )
         {
             $itemToStore = Item::where('name', $newItem['name'])->first();
-            $gift->items()->attach( [ 'item_id' => 1 ], [ 'item_quanity' => 1 ] );
+            //$gift->items()->attach( [ 'item_id' => 1 ], [ 'item_quanity' => 1 ] );
         }
 
     }
