e871814d04336278bd2b119efd17c7677785990c
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-16 21:58:40 -0500

    added donor

--- resources/views/livewire/webhook-confirmation.blade.php
+++ resources/views/livewire/webhook-confirmation.blade.php
@@ -3,9 +3,10 @@
         <div class="text-3xl">Webhook Confirmation</div>
     </div>
     <div>
-        {{ dd($result) }}
-        @foreach ($result['content']['items'] as $item)
+        <!-- @foreach ($result['content']['items'] as $item)
             {{ $item['name'] }}
-        @endforeach
+        @endforeach -->
+
+        {{ $gift }}
     </div>
 </div>
