aab06d7e5eb28c53ff65fa1b2344510e939c03b1
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-18 12:53:20 -0500

    new host page

--- app/Http/Livewire/Store/Catalog.php
+++ app/Http/Livewire/Store/Catalog.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Livewire;
+namespace App\Http\Livewire\Store;
 
 use Livewire\Component;
 use App\Models\Item;
@@ -9,7 +9,7 @@ class Catalog extends Component
 {
     public function render()
     {
-        return view('livewire.catalog')
+        return view('livewire.store.catalog')
             ->withItems(Item::all())
             ->layout('layouts.guest');;
     }
