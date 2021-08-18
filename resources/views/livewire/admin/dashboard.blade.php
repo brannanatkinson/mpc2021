83bc1bec1ccc0454710a2be7a02a9fa6e90f8c3f
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-18 18:24:29 -0500

    new host page

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -1,6 +1,7 @@
 <div>
     @can('admin')
-        You're an admin
+        <div>You're an admin</div>
+        <div>{{ Request::segment(count(request()->segments()) }}</div>
     @elsecan('edit host')
         You're a host --  you're userId is {{ auth()->user()->id }}
     @endcan
