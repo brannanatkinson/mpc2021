6bcc83b8a2f1fb9d123a108145e7798959c43e34
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-18 14:46:47 -0500

    new host page

--- resources/views/livewire/admin/hosts/all-hosts.blade.php
+++ resources/views/livewire/admin/hosts/all-hosts.blade.php
@@ -30,7 +30,9 @@
         @endif
         <div class="mb-4 text-3xl">Create a new host</div>
         <div>
+            @if( $newmessage )
             <div>{{ $newmessage }}</div>
+            @endif
             <form wire:submit.prevent="store">
                 <button type="submit">Save Contact</button>
             </form>
