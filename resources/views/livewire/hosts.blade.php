5ecad0d36d0a10d1babd1bbb983e972c9a4da95d
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-18 10:24:41 -0500

    new host page

--- resources/views/livewire/hosts.blade.php
+++ resources/views/livewire/hosts.blade.php
@@ -6,6 +6,7 @@
         <div class="p-6 bg-gray-100 rounded-md shadow-lg">
             <h2 class="mb-6 text-3xl text-center">{{ $host->name }}</h2>
             <h3 class="mb-4 text-2xl font-bold text-center">${{ $host->totalDonationAmount()->sales }}</h3>
+            <h3 class="mb-4 text-xl text-center">{{ App\Models\Gift::where('host_id', '=', App\Models\Host::where('name', '=', $host->name)->id )->count() }} Gifts</h3>
             <h3 class="mb-4 text-xl text-center">{{ $host->items->sum('pivot.item_quantity') }} Items</h3>
         </div> <!-- end card  -->
         @endforeach
