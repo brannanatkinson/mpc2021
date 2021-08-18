60e1745258f481fbbd0671c7cf186b3099ae1f7f
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-18 17:19:11 -0500

    new host page

--- resources/views/livewire/admin/hosts/all-hosts.blade.php
+++ resources/views/livewire/admin/hosts/all-hosts.blade.php
@@ -3,12 +3,12 @@
     <div class="mb-8 text-4xl text-center">Total for all Hosts – ${{ App\Models\Gift::with('items')->sum('gift_total') }}</div>
     <div class="grid grid-cols-1 lg:grid-cols-4 gap-4">
          @foreach( $hosts as $host )
-        <div class="p-6 bg-gray-100 rounded-md shadow-lg">
-            <h2 class="mb-6 text-3xl text-center">{{ $host->name }}</h2>
-            <h3 class="mb-4 text-2xl font-bold text-center">${{ $host->totalDonationAmount()->sales }}</h3>
             @php
                 $hostId = App\Models\Host::where('name', '=', $host->name)->first()->id 
             @endphp
+        <div class="p-6 bg-gray-100 rounded-md shadow-lg">
+            <h2 class="mb-6 text-3xl text-center">{{ $host->name }}</h2>
+            <h3 class="mb-4 text-2xl font-bold text-center">${{ App\Models\Gift::where('host_id', '=', $hostId )->sum('gift_total') }}</h3>
             <h3 class="mb-4 text-xl text-center">{{ App\Models\Gift::where('host_id', '=', $hostId )->count() }} Gifts</h3>
             <h3 class="mb-4 text-xl text-center">{{ $host->items->sum('pivot.item_quantity') }} Items</h3>
         </div> <!-- end card  -->
