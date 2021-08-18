bd863240fe0a0c8bdd6a4a15dfc8164d78efdfef
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-18 13:46:32 -0500

    new host page

--- resources/views/livewire/admin/hosts/all-hosts.blade.php
+++ resources/views/livewire/admin/hosts/all-hosts.blade.php
@@ -16,6 +16,17 @@
     </div> <!-- end grid  -->
 
     <div class="mt-16">
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <a href="#" class="close" data-dismiss="alert">&times;</a>
+                <strong>Sorry!</strong> invalid input.<br><br>
+                <ul style="list-style-type:none;">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <div class="mb-4 text-3xl">Create a new hosts</div>
         @if($updateMode)
             @include('livewire.admin.hosts.update')
