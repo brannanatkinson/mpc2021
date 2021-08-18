958a21832603327a02a2247667af9c8f0992429d
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-18 14:02:13 -0500

    new host page

--- resources/views/livewire/admin/hosts/all-hosts.blade.php
+++ resources/views/livewire/admin/hosts/all-hosts.blade.php
@@ -29,11 +29,22 @@
             </div>
         @endif
         <div class="mb-4 text-3xl">Create a new hosts</div>
-        @if($updateMode)
-            @include('livewire.admin.hosts.update')
-        @else
-            @include('livewire.admin.hosts.create-host')
-        @endif
+        <div>
+            <div>{{ $name }}</div>
+            <form wire:submit.prevent="what">
+                <div class="grid grid-cols-2">
+                    <div class="">
+                        <label for="">Enter Name</label>
+                        <input type="text" wire:model="name" class="form-control input-sm"  placeholder="Name">
+                    </div>
+                    <div class="">
+                        <label>Enter Email</label>
+                        <input type="email" class="form-control input-sm" placeholder="Enter email" wire:model="email_address">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
     </div>
     
 </div>
