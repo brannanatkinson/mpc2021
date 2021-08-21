d6a277e4854a1b433df9fcffbe3062def8f4abeb
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-20 19:16:18 -0500

    fixing user

--- resources/views/livewire/admin/hosts/all-hosts.blade.php
+++ resources/views/livewire/admin/hosts/all-hosts.blade.php
@@ -4,12 +4,12 @@
     <div class="grid grid-cols-1 lg:grid-cols-4 gap-4">
          @foreach( $hosts as $host )
             @php
-                $hostId = App\Models\Host::where('name', '=', $host->name)->first()->id 
+                $hostId = App\Models\User::where('name', '=', $host->name)->first()->id 
             @endphp
         <div class="p-6 bg-gray-100 rounded-md shadow-lg">
             <h2 class="mb-6 text-3xl text-center">{{ $host->name }}</h2>
-            <h3 class="mb-4 text-2xl font-bold text-center">${{ App\Models\Gift::where('host_id', '=', $hostId )->sum('gift_total') }}</h3>
-            <h3 class="mb-4 text-xl text-center">{{ App\Models\Gift::where('host_id', '=', $hostId )->count() }} Gifts</h3>
+            <h3 class="mb-4 text-2xl font-bold text-center">${{ App\Models\Gift::where('user_id', '=', $hostId )->sum('gift_total') }}</h3>
+            <h3 class="mb-4 text-xl text-center">{{ App\Models\Gift::where('user_id', '=', $hostId )->count() }} Gifts</h3>
             <h3 class="mb-4 text-xl text-center">{{ $host->items->sum('pivot.item_quantity') }} Items</h3>
         </div> <!-- end card  -->
         @endforeach
