de82b3233f9419544ffd9e033cf4469326399e3e
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-20 19:22:30 -0500

    fixing user

--- resources/views/livewire/catalog-item.blade.php
+++ resources/views/livewire/catalog-item.blade.php
@@ -10,6 +10,13 @@
         <img src="{{ Storage::url( $CatalogItem->img ) }}" alt="">
     </div>
     <div class="mt-8">
+        @php 
+            $hosts = App\Models\User::permission('edit host')->get()
+            $hostNames="--|"
+            @foreach ( $hosts as $host )
+                $hostNames += $host->name . '|'
+            @endforeach
+        @endphp
         <button class="snipcart-add-item px-4 py-3 bg-indigo-700 text-white"
             data-item-id="{{ $CatalogItem->name}}"
             data-item-price="{{ $CatalogItem->cost }}"
@@ -17,7 +24,7 @@
             data-item-description="High-quality replica of The Starry Night by the Dutch post-impressionist painter Vincent van Gogh."
             data-item-name="{{ $CatalogItem->name }}"
             data-item-custom1-name="Credit your virtual host"
-            data-item-custom1-options="--|Ralphie Atkinson|Rinkle Atkinson"
+            data-item-custom1-options="{{ $hostNames }}"
             data-item-custom1-value="Rinkle Atkinson">
 
             Add to cart
