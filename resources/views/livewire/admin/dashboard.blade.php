19b6ef35545d48dcd2da5dcce52c24e77c69d947
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-24 10:47:12 -0500

    updating dashboard

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -1,19 +1,28 @@
 <div>
     @can('admin')
-       <div class="my-6 text-3xl text-center">Housing Hope 2021 Dashboard</div>
-       <div class="max-w-5xl mx-auto">
-           <div class="grid grid-cols-3 gap-8">
-               <div class="p-8 text-center bg-gray-100 rounded-md">
-                    ${{ App\Models\Gift::all()->sum('gift_total') }}
-                </div>
-               <div class="p-8 text-center bg-gray-100 rounded-md">
-                    {{ App\Models\Gift::all()->count() }} gifts
-                </div>
-               <div class="p-8 text-center bg-gray-100 rounded-md">
-                    {{ App\Models\Donor::all()->count() }} donors
-                </div>
-           </div>
-       </div>
+        <div class="my-6 text-3xl text-center">Housing Hope 2021 Dashboard</div>
+        <div class="max-w-5xl mx-auto">
+            <div class="grid grid-cols-3 gap-8">
+                <div class="p-8 text-center bg-gray-100 rounded-md">
+                     ${{ App\Models\Gift::all()->sum('gift_total') }}
+                 </div>
+                <div class="p-8 text-center bg-gray-100 rounded-md">
+                     {{ App\Models\Gift::all()->count() }} gifts
+                 </div>
+                <div class="p-8 text-center bg-gray-100 rounded-md">
+                     {{ App\Models\Donor::all()->count() }} donors
+                 </div>
+            </div>
+        </div>
+        <div class="max-w-6xl mx-auto">
+            <div class="grid grid-cols-5 gap 8">
+                @foreach( App\Models\User::permission('edit host')->orderBy('name')->get() as $host)
+                <div class="p-4 cols-span-2">{{ $host->name }}</div>
+                <div class="p-4">${{ $host->totalDonationAmount() }} Raised</div>
+                <div class="p-4">{{ App\Model\Gift::where('user_id', '=', $host->id) }} Gifts</div>
+                <div class="p-4">{{ $host->donatedItems() }} Items</div>
+            </div>
+        </div>
 
 
     @elsecan('edit host')
