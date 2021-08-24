c8901ad0bccb65b7a44cc06fa849fdd54ea05e8b
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-24 10:57:19 -0500

    updating dashboard

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -19,7 +19,6 @@
                 @foreach( App\Models\User::permission('edit host')->orderBy('name')->get() as $host )
                 <div class="p-4 cols-span-2">{{ $host->name }}</div>
                 <div class="p-4">${{ $host->totalDonationAmount() }} Raised</div>
-                <div class="p-4">{{ $host }}</div>
                 @endforeach
             </div>
         </div>
