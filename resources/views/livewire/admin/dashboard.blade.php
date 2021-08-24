a99a92191579a781c5297a16c40232d1934e1e18
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-23 20:49:25 -0500

    fixing gift users

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -1,11 +1,17 @@
 <div>
     @can('admin')
        <div class="my-6 text-3xl text-center">Housing Hope 2021 Dashboard</div>
-       <div class="max-w-5x mx-auto">
+       <div class="max-w-5xl mx-auto">
            <div class="grid grid-cols-3 gap-8">
-               <div class="p-8 text-center">${{ App\Models\Gift::all()->sum('gift_total') }}</div>
-               <div class="p-8 text-center">{{ App\Models\Gift::all()->count() }} gifts</div>
-               <div class="p-8 text-center">{{ App\Models\Donor::all()->count() }} donors</div>
+               <div class="p-8 text-center bg-gray-100 rounded-md">
+                    ${{ App\Models\Gift::all()->sum('gift_total') }}
+                </div>
+               <div class="p-8 text-center bg-gray-100 rounded-md">
+                    {{ App\Models\Gift::all()->count() }} gifts
+                </div>
+               <div class="p-8 text-center bg-gray-100 rounded-md">
+                    {{ App\Models\Donor::all()->count() }} donors
+                </div>
            </div>
        </div>
 
