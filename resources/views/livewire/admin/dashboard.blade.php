3887527a2f75b60547a8dbd7f6b4504616cbda7a
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-24 10:59:50 -0500

    updating dashboard

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -18,7 +18,7 @@
             <div class="grid grid-cols-5 gap 8">
                 @foreach( App\Models\User::permission('edit host')->orderBy('name')->get() as $host )
                 <div class="p-4 cols-span-2">{{ $host->name }}</div>
-                <div class="p-4">${{ $host->totalDonationAmount() }} Raised</div>
+                <div class="p-4">${{ $host->id }} Raised</div>
                 @endforeach
             </div>
         </div>
