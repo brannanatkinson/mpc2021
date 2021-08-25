97769d93eaaf9da40983a28f6ed5d4ada68bc0ad
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-25 18:22:10 -0500

    updating host public page

--- resources/views/livewire/hosts/host-public-page.blade.php
+++ resources/views/livewire/hosts/host-public-page.blade.php
@@ -1,11 +1,16 @@
 <div>
-    <div>Public page for {{ $user->name }}</div>
-    <div>
-        @if(Session::has('host'))
-        <div class="text-3xl">
-          {{ Session::get('host')}}
+    <div class="my-12 text-3xl text-center">
+        {{ $user->name }}
+    </div>
+    <div class="max-w-4xl">
+        <div class="my-6">
+            <div class="text-center uppercase">
+                Amount Raised
+            </div>
+            <div class="text-3xl font-bold text-center">
+                ${{ App\Models\Gift::where('user_id', '=', $hostId )->sum('gift_total') }}
+            </div>
         </div>
-        @endif
 
     </div>
 </div>
