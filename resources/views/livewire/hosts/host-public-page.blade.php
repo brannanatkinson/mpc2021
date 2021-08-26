9adec60d3ec5d6fce6a8be6bc3db559efba73987
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-25 20:54:45 -0500

    updating host public page

--- resources/views/livewire/hosts/host-public-page.blade.php
+++ resources/views/livewire/hosts/host-public-page.blade.php
@@ -15,8 +15,20 @@
             </div>
             <div class="my-8">
                 <div class="relative pt-1">
-                  <div class="overflow-hidden h-4 mb-4 text-xs flex rounded bg-green-200">
-                    <div style="width:@php echo $hostGoalProgress @endphp%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"></div>
+                  <div class="flex mb-2 items-center justify-between">
+                    <div>
+                      <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-green-600 bg-green-200">
+                        Progress Toward My Goal
+                      </span>
+                    </div>
+                    <div class="text-right">
+                      <span class="text-xs font-semibold inline-block text-green-600">
+                        @php echo $hostGoalTotal @endphp%
+                      </span>
+                    </div>
+                  </div>
+                  <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-green-200">
+                    <div style="width:@php echo $hostGoalTotal @endphp%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"></div>
                   </div>
                 </div>
             </div>
