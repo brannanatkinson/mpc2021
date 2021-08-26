85fe305aefcffbec4ceed1da1078b0ecc6de40d8
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-26 07:06:45 -0500

    updating host public page

--- app/Http/Livewire/Admin/Items/AllItems.php
+++ app/Http/Livewire/Admin/Items/AllItems.php
@@ -3,11 +3,23 @@
 namespace App\Http\Livewire\Admin\Items;
 
 use Livewire\Component;
+use App\Models\Item;
 
 class AllItems extends Component
 {
+    public $items;
+    public $updateMode = false;
+
+    public function mount(){
+        $this->items = Itee::orderBy('id')->get();
+    }
     public function render()
     {
         return view('livewire.admin.items.all-items');
+    }
+
+    public function showNewItemForm()
+    {
+        $this->updateMode = true;
     }
 }
--- resources/views/livewire/admin/items/all-items.blade.php
+++ resources/views/livewire/admin/items/all-items.blade.php
@@ -1,3 +1,8 @@
 <div>
     <div wire:click="showNewItemForm" class="px-4 py-3 bg-green text-white">Add new item</div>
+    @if ( $updatedMode == true )
+    <div>Form will go here</div>
+    @else
+    <div>Hide the form</div>
+    @endif
 </div>
