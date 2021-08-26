6bef585e919ae05f26650973340bd5a09939327b
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-26 07:27:03 -0500

    updating host public page

--- resources/views/livewire/admin/items/all-items.blade.php
+++ resources/views/livewire/admin/items/all-items.blade.php
@@ -1,9 +1,18 @@
 <div class="container">
+    <div class="my-12 max-w-5xl">
+        <div class="text-3xl">
+            Giving Catalog Items
+        </div>
+        <div class="grid grid-cols-6 gap-8">
+            @foreach ( $items as $item )
+                <div>Image</div>
+                <div class="col-span-4">{{ $item->name }} </div>
+                <div>Edit</div>
+            @endforeach
+        </div>
+    </div>
     <div wire:click="showNewItemForm" class="inline-flex px-4 py-3 bg-green-800 text-white">Add new item</div>
-    <div>Message = {{ $message }}</div>
     @if ( $updateMode == true )
     @include('livewire.admin.items.new-item')
-    @else
-    <div>Hide the form</div>
     @endif
 </div>
