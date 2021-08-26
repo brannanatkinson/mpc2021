5112a28d750a3987aa37b934377df380fdd6ed36
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-26 09:53:54 -0500

    updating host public page

--- resources/views/livewire/catalog.blade.php
+++ resources/views/livewire/catalog.blade.php
@@ -2,12 +2,17 @@
     <h1 class="text-3xl">2021 Catalog</h1>
     <div class="mt-8 grid grid-cols-4 gap-8 mb-4">
          @foreach ($items as $item)
+
         <div class="overflow-hidden rounded-md">
             <div class="mb-4">
-                <img src="{{ Storage::url( $item->img ) }}" alt="">
+                 <a href="/catalog/item/{{ $item->id}}">
+                    <img src="{{ Storage::url( $item->img ) }}" alt="">
+                </a>
             </div>
             <div class="mb-8">
-                <div class="text-2xl">{{ $item->name }}</div>
+                 <a href="/catalog/item/{{ $item->id}}" class="px-4 py-3 bg-green-500 text-white">
+                    <div class="text-2xl">{{ $item->name }}</div>
+                </a>
                 <div class="">${{ $item->cost }}</div>
             </div>
             <div class="mb-6">
