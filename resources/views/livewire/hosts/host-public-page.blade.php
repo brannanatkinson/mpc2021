649e15b020d38646309e25fa11fac8d3240cddad
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-25 20:51:30 -0500

    updating host public page

--- resources/views/livewire/hosts/host-public-page.blade.php
+++ resources/views/livewire/hosts/host-public-page.blade.php
@@ -15,7 +15,7 @@
             </div>
             <div class="my-8">
                 <div class="relative pt-1">
-                  <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-pink-200">
+                  <div class="overflow-hidden h-4 mb-4 text-xs flex rounded bg-pink-200">
                     <div style="width:@php echo $hostGoalProgress @endphp%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-pink-500"></div>
                   </div>
                 </div>
