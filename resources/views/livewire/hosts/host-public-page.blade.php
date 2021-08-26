8d0c1eab4699b288e8ced5c9ebd9ec74d80a27ad
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-25 21:58:45 -0500

    updating host public page

--- resources/views/livewire/hosts/host-public-page.blade.php
+++ resources/views/livewire/hosts/host-public-page.blade.php
@@ -10,7 +10,10 @@
             <div class="text-6xl font-bold text-center">
                 ${{ App\Models\Gift::where('user_id', '=', $user->id )->sum('gift_total') }}
                 @php
-                    $hostGoalProgress = ( App\Models\Gift::where('user_id', '=', $user->id )->sum('gift_total') / $user->UserMeta->goal ) * 100;
+                    if ( $user->UserMeta->goal ){
+                        $hostGoalProgress = ( App\Models\Gift::where('user_id', '=', $user->id )->sum('gift_total') / $user->UserMeta->goal ) * 100;
+                    }
+                    
                 @endphp 
             </div>
             @if ( $user->UserMeta->show_goal == true)
