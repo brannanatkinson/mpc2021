5b16197e70ee28e22592086eed6bbd6dfdabadc4
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-25 20:30:41 -0500

    updating host public page

--- resources/views/livewire/hosts/host-public-page.blade.php
+++ resources/views/livewire/hosts/host-public-page.blade.php
@@ -11,6 +11,9 @@
                 ${{ App\Models\Gift::where('user_id', '=', $user->id )->sum('gift_total') }}
             </div>
         </div>
+        <div>
+            <x-jet-nav-link href="{{ route('catalog') }}" class="px-4 py-3 text-2xl text-white bg-green-500">Shop the Giving Catalog</x-jet-nav-link>
+        </div>
 
     </div>
 </div>
