52936b803b52809f473b084dccd6d5cd3207222b
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-26 17:37:06 -0500

    updating item backend

--- resources/views/livewire/admin/categories/all-categories.blade.php
+++ resources/views/livewire/admin/categories/all-categories.blade.php
@@ -4,17 +4,17 @@
             Giving Catalog Categories
         </div>
         <div class="grid grid-cols-6 gap-8">
-            @foreach ( $items as $item )
-                <div class="col-span-4"><span clsss="text-2xl">{{ $item->name }}</span> </div>
-                <div><a wire:click.prevent="edit({{ $item->id }})">Edit</a></div>
+            @foreach ( $categories as $category )
+                <div class="col-span-4"><span clsss="text-2xl">{{ $category->name }}</span> </div>
+                <div><a wire:click.prevent="edit({{ $category->id }})">Edit</a></div>
             @endforeach
         </div>
     </div>
     <div wire:click="showNewItemForm" class="inline-flex px-4 py-3 bg-green-800 text-white">Add new category</div>
     @if ( $createMode == true )
-    @include('livewire.admin.items.new-item')
+    @include('livewire.admin.categories.new-item')
     @endif
     @if ( $updateMode == true )
-    @include('livewire.admin.items.update-item')
+    @include('livewire.admin.categories.update-item')
     @endif
 </div>
