ed1a9b631aa825c23ee0a1dc1af54fc8ff8bbc3e
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-26 07:48:53 -0500

    updating host public page

--- resources/views/livewire/admin/items/new-item.blade.php
+++ resources/views/livewire/admin/items/new-item.blade.php
@@ -1,22 +1,20 @@
 <div class="container">
-    <div class="max-w-5xl">
+    <div class="max-w-5xl mx-auto">
         <div class="text-3xl">
             Add a New Item
         </div>
         <form wire:submit.prevent="saveNewItem" class="flex flex-col">
-            <div>
+            <div class="flex flex-col">
                 <label>Item Name</label>
-                <input type="text" class="mb-8 h-16 bg-green-100 rounded-md" wire:model="newItemName">
-            </div>
-            <div>
-                <label>Item Description</label>
+                <input type="text" class="mb-8 h-16 bg-green-100 rounded-md w-full" wire:model="newItemName">
                 @error('newItemName') <span class="error">{{ $message }}</span> @enderror
             </div>
-            <div>
-                <textarea class="mb-8 h-16 bg-green-100 rounded-md" wire:model="newItemDescription"></textarea>
+            <div class="flex flex-col">
+                <label>Item Description</label>
+                <textarea class="mb-8 h-16 bg-green-100 rounded-md w-full" wire:model="newItemDescription"></textarea>
                 @error('newItemDescription') <span class="error">{{ $message }}</span> @enderror
             </div>
-            <button type="submit" class="px-4 py-3 bg-green-800 text-white">Save New Item</button>
+            <button type="submit" class="inline-flex px-4 py-3 bg-green-800 text-white">Save New Item</button>
         </form>
     </div>
 </div>
