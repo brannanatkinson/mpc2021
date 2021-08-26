5788fc51d83f36ac051d0d24eddc3ae1063ff459
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-26 07:22:10 -0500

    updating host public page

--- resources/views/livewire/admin/items/all-items.blade.php
+++ resources/views/livewire/admin/items/all-items.blade.php
@@ -1,5 +1,6 @@
 <div class="container">
     <div wire:click="showNewItemForm" class="inline-flex px-4 py-3 bg-green-800 text-white">Add new item</div>
+    <div>Message = {{ $message }}</div>
     @if ( $updateMode == true )
     @include('livewire.admin.items.new-item')
     @else
