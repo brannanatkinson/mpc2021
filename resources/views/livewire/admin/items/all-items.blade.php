188776835af4a8fd22ec2ede62ed170d91f0f8a3
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-26 07:12:30 -0500

    updating host public page

--- resources/views/livewire/admin/items/all-items.blade.php
+++ resources/views/livewire/admin/items/all-items.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div wire:click="showNewItemForm" class="px-4 py-3 bg-green text-white">Add new item</div>
-    @if ( $updatedMode == true )
+    @if ( $updateMode == true )
     <div>Form will go here</div>
     @else
     <div>Hide the form</div>
