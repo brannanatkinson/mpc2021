fa146c166aa3c5d93f35c2af085cefe507f72b3a
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-26 07:40:01 -0500

    updating host public page

--- resources/views/livewire/admin/items/new-item.blade.php
+++ resources/views/livewire/admin/items/new-item.blade.php
@@ -5,14 +5,14 @@
         </div>
         <form wire:submit.prevent="saveNewItem">
             <lable>Item Name</lable>
-            <input type="text" class="h-16 bg-green-100 rounded-md" wire:model="newItemName">
+            <input type="text" class="mb-8 h-16 bg-green-100 rounded-md" wire:model="newItemName">
             <lable>Item Description</lable>
             @error('newItemName') <span class="error">{{ $message }}</span> @enderror
 
-            <textarea class="h-16 bg-green-100 rounded-md" wire:model="newItemDescription"></textarea>
+            <textarea class="mb-8 h-16 bg-green-100 rounded-md" wire:model="newItemDescription"></textarea>
             @error('newItemDescription') <span class="error">{{ $message }}</span> @enderror
 
-            <button type="submit">Save New Item</button>
+            <button type="submit" class="px-4 py-3 bg-green-800 text-white">Save New Item</button>
         </form>
     </div>
 </div>
