5b39a8fff5bdf2f024d08c9c1bd0a73d4d52a3a6
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-26 09:51:01 -0500

    updating host public page

--- resources/views/livewire/catalog.blade.php
+++ resources/views/livewire/catalog.blade.php
@@ -1,10 +1,10 @@
 <div class="py-12 container mx-auto">
     <h1 class="text-3xl">2021 Catalog</h1>
-    <div class="mt-8 grid grid-cols-3 gap-8 mb-4">
+    <div class="mt-8 grid grid-cols-4 gap-8 mb-4">
          @foreach ($items as $item)
         <div class="overflow-hidden rounded-md">
             <div class="mb-4">
-                <img src="{{ Storage::url( $item->img ) }}" alt="">" alt="">
+                <img src="{{ Storage::url( $item->img ) }}" alt="">
             </div>
             <div class="mb-8 text-2xl">
                 {{ $item->name}}
