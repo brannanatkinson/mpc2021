e3300b21c276ec5a9899206bb85083b8a3ebd8d8
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-26 08:28:40 -0500

    updating host public page

--- resources/views/livewire/catalog-item.blade.php
+++ resources/views/livewire/catalog-item.blade.php
@@ -1,6 +1,6 @@
 <div class="container mx-auto">
     <div class="max-w-4xl mx-auto">
-        <img src="{{ asset('storage/' . $CatalogItem->img ); }}" class="h-full" alt="">
+        <img src="{{ asset('storage/app/' . $CatalogItem->img ) }}" class="h-full" alt="">
     </div>
     <div class="text-3xl">
         {{ $CatalogItem->name }}
