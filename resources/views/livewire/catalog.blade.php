e54d66d7568af21b1054126ce703dff0dbdcc725
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-26 18:27:29 -0500

    updating item backend

--- resources/views/livewire/catalog.blade.php
+++ resources/views/livewire/catalog.blade.php
@@ -1,31 +1,38 @@
 <div class="py-12 container mx-auto">
     <h1 class="text-3xl">2021 Giving Catalog</h1>
     @foreach ( App\Models\Category::orderBy('id')->get() as $category )
-    <div class="text-3xl">
-        {{ $category->name }}
-    </div>
-    <div class="mt-8 grid grid-cols-4 gap-8 mb-4">
-         @foreach ( $category->items as $item)
-        <div class="overflow-hidden rounded-md">
-            <div class="mb-4">
-                 <a href="/catalog/item/{{ $item->id}}">
-                    <img src="{{ Storage::url( $item->img ) }}" alt="">
-                </a>
+    <div class="grid grid-cols-2 gap-8">
+        <div>
+             <div class="text-3xl">
+                {{ $category->name }}
             </div>
-            <div class="mb-8">
-                 <a href="/catalog/item/{{ $item->id}}">
-                    <div class="text-2xl">{{ $item->name }}</div>
-                </a>
-                <div class="">${{ $item->cost }}</div>
-            </div>
-            
-            <div class="mb-6">
-                <a href="/catalog/item/{{ $item->id}}" class="px-4 py-3 bg-green-500 text-white">
-                    Details
-                </a>
+            <div>{{ $category->description }}</div>
+        </div>
+        <div>
+            <div class="mt-8 grid grid-cols-2 gap-8 mb-4">
+                 @foreach ( $category->items as $item)
+                <div class="overflow-hidden rounded-md">
+                    <div class="mb-4">
+                         <a href="/catalog/item/{{ $item->id}}">
+                            <img src="{{ Storage::url( $item->img ) }}" alt="">
+                        </a>
+                    </div>
+                    <div class="mb-8">
+                         <a href="/catalog/item/{{ $item->id}}">
+                            <div class="text-2xl">{{ $item->name }}</div>
+                        </a>
+                        <div class="">${{ $item->cost }}</div>
+                    </div>
+                    
+                    <div class="mb-6">
+                        <a href="/catalog/item/{{ $item->id}}" class="px-4 py-3 bg-green-500 text-white">
+                            Details
+                        </a>
+                    </div>
+                </div>
+                @endforeach
             </div>
         </div>
-        @endforeach
     </div>
     @endforeach
 </div>
