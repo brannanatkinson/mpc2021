550b86d915ac253f6969c18e73dd771e3cc76ea5
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-25 20:36:01 -0500

    updating host public page

--- resources/views/livewire/hosts/host-public-page.blade.php
+++ resources/views/livewire/hosts/host-public-page.blade.php
@@ -11,6 +11,12 @@
                 ${{ App\Models\Gift::where('user_id', '=', $user->id )->sum('gift_total') }}
             </div>
         </div>
+        <div class="my-6">
+            <div class="mb-8text-3xl text-center">Why I'm supporting Housing Hope</div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed convallis tristique sem.  Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. 
+            </p>
+        </div>
         <div class="text-center">
             <button href="{{ route('catalog') }}" class="px-4 py-3 text-2xl text-white bg-green-500">Shop the Giving Catalog</button>
         </div>
