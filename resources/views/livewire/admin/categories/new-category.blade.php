608d07a3e18ad03e8eda29b019a070608bc5c9ba
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-26 22:30:18 -0500

    updating item backend

--- resources/views/livewire/admin/categories/new-category.blade.php
+++ resources/views/livewire/admin/categories/new-category.blade.php
@@ -1,47 +1,19 @@
 <div class="container">
     <div class="max-w-5xl mx-auto">
         <div class="text-3xl">
-            Add a New Sponsor
+            Add a New Category
         </div>
-        <form wire:submit.prevent="saveNewSponsor" class="flex flex-col">
+        <form wire:submit.prevent="saveNewCategory" class="flex flex-col">
             <div class="flex flex-col">
-                <label>Sponsor Name</label>
-                <input type="text" class="mb-8 h-16 bg-green-100 rounded-md w-full" wire:model="name">
+                <label>Category Name</label>
+                <input type="text" class="h-8 bg-green-100 rounded-md w-full" wire:model="name">
                 @error('name') <span class="error">{{ $message }}</span> @enderror
             </div>
-            <select name="" id="" wire:model="category">
-                <option value="title">Title</option>
-                <option value="catalog">Giving Catalog</option>
-                <option value="wall">Giving Wall</option>
-                <option value="champion">Champion of Hope</option>
-                <option value="matching">Matching</option>
-                <option value="promoter">Promoter of Hope</option>
-                <option value="advocate">Advocate of Hope</option>
-                <option value="bearer">Bearer of Hope</option>
-                <option value="supporter">Supporter of Hope</option>
-            </select>
-            @if ( $category == 'matching' )
             <div class="flex flex-col">
-                <label>Sponsor match</label>
-                <input type="number" class="mb-8 h-16 bg-green-100 rounded-md w-full" wire:model="match">
+                <label>Category Description</label>
+                <textarea class="bg-green-100 rounded-md w-full" wire:model="description"></textarea>
             </div>
-            <div class="flex flex-col">
-                <label>Matched Gift</label>
-                <select name="" id="" wire:model="gift">
-                    @foreach( App\Models\Gift::orderBy('id')->get() as $gift)
-                    <option value="{{ $gift->id }}">{{ $gift->name }}</option>
-                    @endforeach
-            </div>
-            @endif
-            <div class="flex flex-col">
-                <label>Sponsor Website</label>
-                <input type="text" class="mb-8 h-16 bg-green-100 rounded-md w-full" wire:model="website">
-            </div>
-            <div class="flex flex-col">
-                <label>Sponsor Image</label>
-                <input type="file" wire:model="image"></textarea>
-            </div>
-            <button type="submit" class="inline-flex px-4 py-3 bg-green-800 text-white">Save New Sponsor</button>
+            <button type="submit" class="inline-flex px-4 py-3 bg-green-800 text-white">Save New Category</button>
         </form>
     </div>
 </div>
