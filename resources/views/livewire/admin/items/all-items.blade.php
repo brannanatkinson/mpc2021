b729a8841bfa79b1330b6b436f8a01ec1f2c1c26
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-26 19:57:03 -0500

    updating item backend

--- resources/views/livewire/admin/items/all-items.blade.php
+++ resources/views/livewire/admin/items/all-items.blade.php
@@ -6,7 +6,7 @@
         <div class="grid grid-cols-2 gap-6">
             @foreach ( $items as $item )
             <div class="h-24 flex flex-row items-center">
-                <img src="{{ Storage::url( $item->img ) }}" alt="">
+                <img src="{{ Storage::url( $item->img ) }}" alt="" class="object-fit">
                 <div><span clsss="pl-8 text-2xl">{{ $item->name }}</span> – <a wire:click.prevent="edit({{ $item->id }})">Edit</a></div>
             </div>  
             @endforeach
