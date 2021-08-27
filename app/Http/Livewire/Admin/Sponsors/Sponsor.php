4e4e0a1e723e12258d4ae5e96032ed41bda58a57
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-26 21:51:38 -0500

    updating item backend

--- app/Http/Livewire/Admin/Sponsors/Sponsor.php
+++ app/Http/Livewire/Admin/Sponsors/Sponsor.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace App\Http\Livewire\Admin\Sponsos;
+namespace App\Http\Livewire\Admin\Sponsors;
 
 use Livewire\Component;
 
 class Sponsor extends Component
 {
+    public $name, $category, $match, $gift, $image, $website, 
     public function render()
     {
         return view('livewire.admin.sponsos.sponsor');
