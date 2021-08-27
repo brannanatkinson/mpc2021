463635ddd850bc7363f784a3cd6d7672919a9996
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-26 22:23:38 -0500

    updating item backend

--- app/Http/Livewire/Admin/Sponsors/AllSponsors.php
+++ app/Http/Livewire/Admin/Sponsors/AllSponsors.php
@@ -55,7 +55,7 @@ class AllSponsors extends Component
             $photoPath = null;
         }
        
-        Category::create([
+        Sponsor::create([
             'name' => $this->name,
             'category' => $this->category,
             'match' => $this->match,
