59958f2bd65beb2372ace414785fefc5cee95c42
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-26 22:30:55 -0500

    updating item backend

--- resources/views/livewire/admin/categories/new-category.blade.php
+++ resources/views/livewire/admin/categories/new-category.blade.php
@@ -4,12 +4,12 @@
             Add a New Category
         </div>
         <form wire:submit.prevent="saveNewCategory" class="flex flex-col">
-            <div class="flex flex-col">
+            <div class="mt-8 mb-6 flex flex-col">
                 <label>Category Name</label>
                 <input type="text" class="h-8 bg-green-100 rounded-md w-full" wire:model="name">
                 @error('name') <span class="error">{{ $message }}</span> @enderror
             </div>
-            <div class="flex flex-col">
+            <div class="mb-6 flex flex-col">
                 <label>Category Description</label>
                 <textarea class="bg-green-100 rounded-md w-full" wire:model="description"></textarea>
             </div>
