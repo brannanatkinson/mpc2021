c80d65e43c7fa454c0e84569199272a1b676b110
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-28 21:50:55 -0500

    fixing host page

--- resources/views/livewire/admin/hosts/update-host-form.blade.php
+++ resources/views/livewire/admin/hosts/update-host-form.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="max-w-5xl">
+    <div class="max-w-5xl mx-auto">
         <div class="mb-8">Profile for {{ $user->name }}</div>
         <div class="mb-8">
             <p>This form will help you update your public Housing Hope profile.</p>
@@ -7,11 +7,25 @@
         <div class="mb-8">
             <div class="text-2xl">Show Host Totals</div>
             <p>This is the total amound of donations where users have credited you as the virtual hosts</p>
-            <label for="">Check to show donation total</label>
             <input type="checkbox">
+            <label for="">Check to show donation total</label>
         </div>
         <div class="mb-8">
-            
+            <div class="text-2xl">Show Your Goal</div>
+            <p>Enter an amount if you would like to set a goal for the amount you want to raise. Please leave blank if you don't want to show a goal.</p>
+            <label for="">Enter your goal amount</label>
+            <input type="text">
+        </div>
+        <div class="mb-8">
+            <div class="text-2xl">Show Donated Items</div>
+            <p>This is option will show users the items that people have donated when crediting you as the host.</p>
+            <input type="checkbox">
+            <label for="">Check to show items</label>
+        </div>
+        <div class="mb-8">
+            <div class="text-2xl">Show Reason for Supporting Housing Hope</div>
+            <p>Write a statement about why you support The Mary Parrish Center or any message you want visitiors to see.</p>
+            <textarea></textarea>
         </div>
     </div>
     
