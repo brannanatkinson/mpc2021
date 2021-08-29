9799e53423c2b58e9a52e775ee7861a221e2cb7b
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-29 13:48:07 -0500

    updating host options

--- resources/views/livewire/admin/hosts/update-host-form.blade.php
+++ resources/views/livewire/admin/hosts/update-host-form.blade.php
@@ -6,8 +6,8 @@
         </div>
         @endif
         @if ( $image )
-        <div class="mb-8 h-24 w-24 rounded-full">
-            <img src="{{ Storage::url( $image ) }}" class="object-cover" alt="">
+        <div class="mb-8 h-24 w-24 rounded-full overflow-hidden">
+            <img src="{{ Storage::url( $image ) }}" class="mx-auto object-cover" alt="">
         </div>
         @endif
         <div class="mb-8">
