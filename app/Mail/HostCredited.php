05ddd229a8fa9e806fbbdc1e9873bfb441b63150
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-29 18:55:54 -0500

    updating host options

--- app/Mail/HostCredited.php
+++ app/Mail/HostCredited.php
@@ -31,6 +31,6 @@ class HostCredited extends Mailable
      */
     public function build()
     {
-        return $this->subject($this->gift->donor->full_name . ' credited as a host')->view('mail.hostcredited');
+        return $this->subject($this->gift->donor->full_name . ' you credited as a host')->view('mail.hostcredited');
     }
 }
--- resources/views/mail/hostcredited.blade.php
+++ resources/views/mail/hostcredited.blade.php
@@ -1,9 +1,13 @@
 <div>
-    <div>{{ $gift->donor->full_name }} just made a donation in the amount of ${{ $gift->gift_total }} and credited you. </div>
-    <div>
+    <div class="mb-6">
+        {{ $gift->donor->full_name }} just made a ${{ $gift->gift_total }} gift to The Mary Parrish Center and credited you as the host.
+    </div>
+    <div class="mb-6 text-3xl">
         Donation Summary
     </div>
     @foreach( $gift->items as $item )
-    <div>{{ $item->name }} - {{ $item->pivot->item_quantity }}</div>
+    <div class="mb-4 text-2xl">
+        {{ $item->name }} - {{ $item->pivot->item_quantity }}
+    </div>
     @endforeach
 </div>
