0a0c1392e5b9a1a10a6eda291a1ced777237ce97
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-28 21:45:52 -0500

    fixing host page

--- resources/views/livewire/admin/hosts/update-host-form.blade.php
+++ resources/views/livewire/admin/hosts/update-host-form.blade.php
@@ -1,3 +1,18 @@
 <div>
-    {{ $user->name }}
+    <div class="max-w-5xl">
+        <div class="mb-8">Profile for {{ $user->name }}</div>
+        <div class="mb-8">
+            <p>This form will help you update your public Housing Hope profile.</p>
+        </div>
+        <div class="mb-8">
+            <div class="text-2xl">Show Host Totals</div>
+            <p>This is the total amound of donations where users have credited you as the virtual hosts</p>
+            <label for="">Check to show donation total</label>
+            <input type="checkbox">
+        </div>
+        <div class="mb-8">
+            
+        </div>
+    </div>
+    
 </div>
