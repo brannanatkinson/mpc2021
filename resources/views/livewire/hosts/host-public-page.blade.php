de3b53edafa86d6e1e9b8df2e9fc4b20996a566d
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-29 13:08:42 -0500

    updating host options

--- resources/views/livewire/hosts/host-public-page.blade.php
+++ resources/views/livewire/hosts/host-public-page.blade.php
@@ -4,9 +4,11 @@
     </div>
     <div class="max-w-4xl mx-auto">
         <div class="mt-6 mb-12">
+            @if ( $user->UserMeta->show_total == true )
             <div class="mb-6 text-center uppercase">
                 Amount Raised
             </div>
+            @endif
             <div class="mb-12 text-6xl text-center font-display">
                 ${{ App\Models\Gift::where('user_id', '=', $user->id )->sum('gift_total') }}
                 @php
@@ -45,6 +47,7 @@
             </div>
             @endif
         </div>
+        @if ( $user->UserMeta->show_items == true )
         <div class="mb-12">
             <div class="mb-4 text-xl text-center uppercase">Thanks to my family and friends for giving these gifts</div>
             @if( $user->donatedItems()->count() > 0 )
@@ -60,12 +63,15 @@
             </div>
             @endif
         </div>
+        @endif
+        @if ( $user->UserMeta->show_rationale == true )
         <div class="mb-16 bg-white rounded-md">
             <div class="mb-8 text-3xl text-center">Why I'm supporting Housing Hope</div>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed convallis tristique sem.  Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. 
+                {{ $user->UserMeta->rationale }}
             </p>
         </div>
+        @endif
     </div>
     <div id="mpc" class="bg-mp-coral py-16">
         <div class="container mx-auto px-4 lg:px-0">
