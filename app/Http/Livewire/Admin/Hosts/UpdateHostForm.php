1342117225e7cc4fc48d617bbe144bd5c244732f
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-28 21:37:35 -0500

    fixing host page

--- app/Http/Livewire/Admin/Hosts/UpdateHostForm.php
+++ app/Http/Livewire/Admin/Hosts/UpdateHostForm.php
@@ -8,10 +8,7 @@ use App\Models\User;
 class UpdateHostForm extends Component
 {
     public $user;
-    public function mount()
-    {
-        $this->user = User::find( auth()->user->id );
-    }
+    
     public function render()
     {
         return view('livewire.admin.hosts.update-host-form');
