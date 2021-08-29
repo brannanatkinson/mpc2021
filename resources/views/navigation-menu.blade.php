3511b65df4b2180e8d01fbcff638b87e18f55755
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-29 13:29:55 -0500

    updating host options

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -96,7 +96,7 @@
                                 {{ __('Manage Account') }}
                             </div>
 
-                            <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                            <x-jet-dropdown-link href="{{ route('admin.update.hosts') }}">
                                 {{ __('Profile') }}
                             </x-jet-dropdown-link>
 
