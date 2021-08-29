ddcc70b14e4c801574e8df426e40c2eeaa9696bc
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-29 18:32:25 -0500

    updating host options

--- resources/views/mail/hostcredited.blade.php
+++ resources/views/mail/hostcredited.blade.php
@@ -1,3 +1,9 @@
 <div>
-    {{ $gift->donor->full_name }} just made a donation and credited you.
+    <div>{{ $gift->donor->full_name }} just made a donation in the amount of {{ $gift->gift_total }} and credited you. </div>
+    <div>
+        Donation Summary
+    </div>
+    @foreach( $gift->items as $item )
+    <div>{{ $item->item_name }} - {{ $item->quantity }}</div>
+    @endforeach
 </div>
