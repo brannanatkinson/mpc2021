15ecad1969a0a5c39db8fe7a46b75ac3cada466c
Brannan Atkinson <brannanatkinson@gmail.com>
2021-08-30 21:40:00 -0500

    building confirmation page

--- app/Http/Livewire/OrderConfirmation.php
+++ app/Http/Livewire/OrderConfirmation.php
@@ -7,13 +7,19 @@ use App\Models\Gift;
 
 class OrderConfirmation extends Component
 {
-    public $gift;
+    public $gift, $showNameOnWall, $note, $donorUpdatedName, $hostToCredit;
     public function mount($order_token)
     {
         $this->gift = Gift::where('order_token', '=', $order_token)->first();
+        $this->showNameOnWall = 0;
+        $this->donorUpdatedName = $this->gift->donor->full_name;
     }
     public function render()
     {
         return view('livewire.order-confirmation')->layout('layouts.guest');
+    }
+    public function showDonorName()
+    {
+        $this->showNameOnWall = !$this->showNameOnWall;
     }
 }
--- resources/views/livewire/order-confirmation.blade.php
+++ resources/views/livewire/order-confirmation.blade.php
@@ -1,3 +1,49 @@
 <div>
-    {{ $gift->donor->full_name }} | {{ $gift->order_token }}
+    <x-slot name="title">
+        Order Confirmation
+    </x-slot>
+    <div class="max-w-5xl mx-auto mt-12">
+        <div class="mb-6 text-center text-4xl">You're a lifesaver.</div>
+        <p class="mb-12 text-xl">Thank you for supporting Housing Hope. Your gift helps ensure that residents of The Mary Parrish Center and other survivors of interpersonal violence have an opportunity to be safe, find healing and rebuild their lives.</p>
+        <div class="mb-12 bg-gray-100 p-8">
+            <div class="mb-3 text-2xl">
+                Would you like to add your name to the Giving Wall?
+            </div>
+            <p class="mb-6">The Giving Wall presented by Pinnacle Financial Partners is a place where we recognize the amazing donors like you. If you would like to include your name on the giving wall, please check the box below.</p>
+            <input type="checkbox" class="h-8 w-8" value="0" wire:model="showNameOnWall" wire:click.prevent="showDonorName">
+            <label class="ml-4" for="" wire:model="showNameOnWall">Yes, please add my name to the Giving Wall</label>
+            @if( $showNameOnWall == 1 )
+                <form action="" class="mt-6">
+                    <label for="">The name that will appear on the Giving Wall is <i>{{ $gift->donor->full_name }}</i>. To change your name on the Giving Wall, enter a new name in the box and click the 'Update Name' button.</label>
+                    <div class="mt-4">
+                    <input type="text" class="w-full md:w-1/2" value="{{ $gift->donor->full_name }}" wire:model="donorUpdatedName">
+                    <button class="md:ml-4 px-4 py-3 text-white bg-mp-blue-green">Update Name</button>
+                    </div>
+                </form>
+            @endif
+        </div>
+        <div class="mb-12 bg-gray-100 p-8">
+            <div class="mb-3 text-2xl">
+                Would you like to write a note of support?
+            </div>
+            <p class="mb-6">The Mary Parrish Center clients would love to hear from you. Your anonymous note will also appear on the Giving Wall.</p>
+            <textarea name="" id="" class="w-full mb-4" rows="5"></textarea>
+            <button class="px-4 py-3 text-white bg-mp-blue-green">Save My Note</button>
+        </div>
+        @if( $gift->user_id == null)
+        <div class="mb-12 bg-gray-100 p-8">
+            <div class="mb-3 text-2xl">
+                Would you like to credit a virtual host?
+            </div>
+            <p class="mb-4">We noticed you left the virtual host blank at checkout. Virtual hosts are friends of The Mary Parrish Center helping us spread the word about Housing Hope.</p>
+            <p class="mb-6">You can still credit your virtual host. Please select his or her name from the list below. (Hosts listed alphabetically by first name) </p>
+            <select name="" id="" wire:model="hostToCredit">
+                @foreach( App\Models\User::permission('edit host')->orderBy('name')->get() as $host)
+                <option value="{{ $host->id }}">{{ $host->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        @endif
+    </div>
+
 </div>
