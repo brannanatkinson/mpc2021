b073217296d1e7965b7a187d17b08151208eae3a
Brannan Atkinson <brannanatkinson@gmail.com>
2021-09-04 17:33:38 -0500

    creating sponsor page

--- resources/views/sponsors.blade.php
+++ resources/views/sponsors.blade.php
@@ -1,4 +1,7 @@
 <x-public-layout>
-	<div class="mb-12 text-center text-4xl">Housing Hope Sponsors</div>
+	<div class="mb-6 text-center text-4xl font-display">Housing Hope Sponsors</div>
+	<div class="max-w-4xl mx-auto mb-12 text-xl">
+		The Mary Parrish center is greatful to all our 2021 Housing Hope Sponsors! Combined they have contributed a total of ${{ App\Models\Sponsor::all()->sum('amount') }} toward our work assisting survivors of interpersonal violence and helped make Housing Hope such a succesful event.
+	</div>
 	@livewire('sponsor-grid')
 </x-public-layout>
