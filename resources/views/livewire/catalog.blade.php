05a809659c65b297c9caab101b4e8cc5024404fa
Brannan Atkinson <brannanatkinson@gmail.com>
2021-09-04 14:04:41 -0500

    added password, updated sponsor

--- resources/views/livewire/catalog.blade.php
+++ resources/views/livewire/catalog.blade.php
@@ -1,17 +1,17 @@
 <div class="py-12 container mx-auto">
     <h1 class="text-3xl">2021 Giving Catalog</h1>
     @foreach ( App\Models\Category::orderBy('id')->get() as $category )
-    <div class="max-w-7xl mx-auto flex flex-row">
-        <div class="w-1/4 ">
+    <div class="max-w-7xl mx-auto flex flex-col">
+        <div class="">
             <div class="h-full pt-16 px-12 flex flex-col">
                 <div class="text-3xl">
                     {{ $category->name }}
                 </div>
-                <div>{{ $category->description }}</div>
+                
             </div>
         </div>
-        <div class="w-3/4">
-            <div class="mt-8 grid grid-cols-2 gap-12 mb-4">
+        <div class="">
+            <div class="mt-8 grid grid-cols-4 gap-8 mb-4">
                  @foreach ( $category->items as $item )
                 <div class="flex flex-col overflow-hidden rounded-md">
                     <div class="mb-4">
@@ -39,6 +39,7 @@
                 @endforeach
             </div>
         </div>
+        <div>{{ $category->description }}</div>
     </div>
     @endforeach
 </div>
