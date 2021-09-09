6aa8423c670ef4142b6befc845b54e227891fa80
Brannan Atkinson <brannanatkinson@gmail.com>
2021-09-09 09:16:59 -0500

    disable store buttons

--- resources/views/livewire/catalog-item.blade.php
+++ resources/views/livewire/catalog-item.blade.php
@@ -1,5 +1,19 @@
 <x-public-navigation/>
+@php
+    $userTimezone = new DateTimeZone('America/Chicago');
+    $gmtTimezone = new DateTimeZone('GMT');
+    $myDateTime = new DateTime( date('Y-m-d H:i:s'), $gmtTimezone);
+    $offset = $userTimezone->getOffset($myDateTime);
+    $myInterval=DateInterval::createFromDateString((string)$offset . 'seconds');
+    $myDateTime->add($myInterval);
+    $result = $myDateTime->format('Y-m-d H:i:s');
+    $showOnPage = $result > date( env('START_DATE') );
+@endphp
+@if ( $showOnPage == 0)
+<div class="py-6 mb-6 text-center font-display bg-mp-light-lime text-mp-navy">Housing Hope is preview only until Sept. 13, 2021. Thank you for supporting The Mary Parrish Center.</div>
+@endif
 <div class="container mx-auto">
+    
     <div class="max-w-7xl mx-auto">
         <div class="flex flex-row">
             <div class="w-3/5">
@@ -33,7 +47,7 @@
                             }
                         @endphp
 
-    
+                    @if( $showOnPage == 1 )
                     <button class="snipcart-add-item px-4 py-3 bg-indigo-700 text-white"
                         data-item-id="{{ $CatalogItem->name}}"
                         data-item-price="{{ $CatalogItem->cost }}"
@@ -45,6 +59,7 @@
                         data-item-custom1-value="@php echo $hostToCredit @endphp">
                         Add to cart
                     </button>
+                    @endif
                 </div>
             </div>
         </div>
