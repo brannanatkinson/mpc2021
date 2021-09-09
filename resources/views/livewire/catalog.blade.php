b373e9c0f3a2686278a9b6a8148199741fe0eab7
Brannan Atkinson <brannanatkinson@gmail.com>
2021-09-08 20:31:26 -0500

    updating catalog grid

--- resources/views/livewire/catalog.blade.php
+++ resources/views/livewire/catalog.blade.php
@@ -4,7 +4,7 @@
             Giving Catalog
         </x-slot>
     <div class="mb-12">
-        <img src="{{ Storage::url('/logos/giving_catalog_banner.jpg') }}" class="w-1/2 mx-auto" alt="">
+        <img src="{{ Storage::url('/logos/giving_catalog_banner.jpg') }}" class="w-full md:w-1/2 mx-auto" alt="">
     </div>
     @foreach ( App\Models\Category::orderBy('id')->get() as $category )
     <div class="max-w-7xl mx-auto flex flex-col">
@@ -33,7 +33,7 @@
                     <div class="mb-2 h-16 text-2xl ">
                         {{ $item->name }}
                     </div>
-                    <div class="mb-4 flex-grow">
+                    <div class="mb-4 md:flex-grow">
                         {{ $item->excerpt }}
                     </div>
                     <div class="mb-4 text-3xl">${{ $item->cost }}</div>
--- resources/views/wall.blade.php
+++ resources/views/wall.blade.php
@@ -1,6 +1,6 @@
 <x-public-layout>
 	<div class="mb-12">
-		<img src="{{ Storage::url('/logos/giving_wall_banner.jpg') }}" class="w-1/2 mx-auto" alt="">
+		<img src="{{ Storage::url('/logos/giving_wall_banner.jpg') }}" class="w-full md:w-1/2 mx-auto" alt="">
 	</div>
 	@livewire('results')
 </x-public-layout>
