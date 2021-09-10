cd493e8d7da902cfcb0c4f1f78c70c772866182b
Brannan Atkinson <brannanatkinson@gmail.com>
2021-09-10 11:28:24 -0500

    testing dev

--- resources/views/livewire/hosts/host-public-page.blade.php
+++ resources/views/livewire/hosts/host-public-page.blade.php
@@ -4,19 +4,19 @@
     </div>
     <div class="max-w-4xl mx-auto">
         @if ( $user->profile_photo_path != null )
-        <div class="mb-8 mx-auto h-36 w-36 rounded-full overflow-hidden">
-            <img src="{{ Storage::url( $user->profile_photo_path ) }}" class="object-cover" alt="">
-        </div>
-        @endif
+            <div class="mb-8 mx-auto h-36 w-36 rounded-full overflow-hidden">
+                <img src="{{ Storage::url( $user->profile_photo_path ) }}" class="object-cover" alt="">
+            </div>
+            @endif
         <div class="mt-6 mb-12">
             @if ( $user->UserMeta->show_total == true )
-            <div class="mb-6 text-center uppercase">
-                Amount Raised
-            </div>
-            
-            <div class="mb-12 text-6xl text-center text-mp-blue-green font-display">
-                ${{ App\Models\Gift::where('user_id', '=', $user->id )->sum('gift_total') }}
-            </div>
+                <div class="mb-6 text-center uppercase">
+                    Amount Raised
+                </div>
+                
+                <div class="mb-12 text-6xl text-center text-mp-blue-green font-display">
+                    ${{ App\Models\Gift::where('user_id', '=', $user->id )->sum('gift_total') }}
+                </div>
             @endif
             <div class="mb-12">
                 <div class="text-center">
@@ -34,28 +34,28 @@
                 }
             @endphp 
             @if ( $user->UserMeta->show_goal == true )
-            <div class="mb-8">
-                <div class="relative pt-1">
-                    <div class="mt-6 mb-4 text-xl text-center">
-                        Please help me reach my goal of raising <span class="text-green-700 font-bold">${{ $user->UserMeta->goal }}</span> for The Mary Parrish Center
-                    </div>
-                    <div class="overflow-hidden h-4 mb-4 text-xs flex rounded-full bg-gray-200">
-                        <div style="width:@php echo $hostGoalProgress @endphp%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-mp-blue-green"></div>
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <div>
-                          <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-white bg-mp-blue-green">
-                            Progress Toward My Goal
-                          </span>
+                <div class="mb-8">
+                    <div class="relative pt-1">
+                        <div class="mt-6 mb-4 text-xl text-center">
+                            Please help me reach my goal of raising <span class="text-green-700 font-bold">${{ $user->UserMeta->goal }}</span> for The Mary Parrish Center
                         </div>
-                        <div class="text-right">
-                          <span class="text-xs font-semibold inline-block text-mp-blue-green">
-                            @php echo number_format($hostGoalProgress, 0) @endphp%
-                          </span>
+                        <div class="overflow-hidden h-4 mb-4 text-xs flex rounded-full bg-gray-200">
+                            <div style="width:@php echo $hostGoalProgress @endphp%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-mp-blue-green"></div>
                         </div>
-                  </div>
+                        <div class="flex items-center justify-between">
+                            <div>
+                              <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-white bg-mp-blue-green">
+                                Progress Toward My Goal
+                              </span>
+                            </div>
+                            <div class="text-right">
+                              <span class="text-xs font-semibold inline-block text-mp-blue-green">
+                                @php echo number_format($hostGoalProgress, 0) @endphp%
+                              </span>
+                            </div>
+                      </div>
+                    </div>
                 </div>
-            </div>
             @endif
         </div>
         @if ( $user->UserMeta->show_items == true )
