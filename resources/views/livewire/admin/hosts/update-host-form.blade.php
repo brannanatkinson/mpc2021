5c69eaf2d73af86a9d8f58c31796b108d9d4c495
Brannan Atkinson <brannanatkinson@gmail.com>
2021-09-11 14:51:16 -0500

    photo validation

--- resources/views/livewire/admin/hosts/update-host-form.blade.php
+++ resources/views/livewire/admin/hosts/update-host-form.blade.php
@@ -21,6 +21,7 @@
         </div>
         <div class="mb-12 p-8 bg-white">
             <div class="text-2xl">Add Your Photo</div>
+            <div class="">Your photo must be a JPG, JPEG, or PNG that is less than 1MB in size.</div>
             <div class="mb-4 flex flex-col">
                 <label class="mb-4">Add a photo that will show on your public profile.</label>
                 @error('image') <div class="pb-4"><span class="text-red-500">{{ $message }}</span></div> @enderror
