82ea5ffa237a06e8e577f434b8ce0ecd4dc39a38
Brannan Atkinson <brannanatkinson@gmail.com>
2021-09-12 13:48:25 -0500

    homepage update

--- resources/views/livewire/catalog-item.blade.php
+++ resources/views/livewire/catalog-item.blade.php
@@ -78,7 +78,7 @@
     </div>
 </div>
 @if( $CatalogItem->sponsor_id != null  )
-<div class="mt-8 max-w-5xl mx-auto">
+<div class="mt-8 max-w-5xl mx-auto px-6 md:px-0">
     <div class="flex flex-col md:flex-row items-center p-8 bg-white border border-2 shadow-lg">
         <div class="w-full md:w-1/3">
             <img src="{{ Storage::url( $CatalogItem->sponsor->img ) }}" class="h-48 object-contain" alt="">
