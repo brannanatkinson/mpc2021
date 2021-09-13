9a5e77def5357fe683184a1d7a1ed777ed4ff0ea
Brannan Atkinson <brannanatkinson@gmail.com>
2021-09-12 20:47:45 -0500

    final commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -78,6 +78,7 @@
                 </div>
             </div>
         </div>
+        <div class="py-12 bg-mp-navy text-white text-sm text-center">Housing Hope Nashville - &copy; 2021 The Mary Parrish Center - Photo Credit: Peyton Hoge - Website Development: Amy Atkinson Communications</div>
         <script async src="https://cdn.snipcart.com/themes/v3.2.1/default/snipcart.js"></script>
         <div hidden id="snipcart" data-api-key="{{ env('SNIPCART_KEY') }}"></div>
     </body>
--- resources/views/sponsors.blade.php
+++ resources/views/sponsors.blade.php
@@ -4,7 +4,7 @@
     </x-slot>
 	<div class="mb-6 text-center text-4xl font-display">Housing Hope Sponsors</div>
 	<div class="max-w-4xl mx-auto mb-12 px-6 md:px-0 text-xl">
-		The Mary Parrish Center is greatful to all our 2021 Housing Hope Sponsors! Combined they have contributed a total of <span class="text-mp-blue-green">${{ number_format(App\Models\Sponsor::all()->sum('amount'), 0, ',') }}</span> toward our work assisting survivors of interpersonal violence and helped make Housing Hope such a succesful event.
+		The Mary Parrish Center is greatful to all our 2021 Housing Hope Sponsors! Combined they have contributed a total of <span class="text-mp-blue-green">${{ number_format(App\Models\Sponsor::all()->sum('amount') + 1000, 0, ',') }}</span> toward our work assisting survivors of interpersonal violence and helped make Housing Hope such a succesful event.
 	</div>
 	@livewire('sponsor-grid')
 	@livewire('matching-sponsor-grid')
