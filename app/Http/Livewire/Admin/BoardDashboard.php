e603074de6f0f7e69ebe3ae02cce3d3ac3bcf640
Brannan Atkinson <brannanatkinson@gmail.com>
2021-09-12 21:23:10 -0500

    final commit

--- app/Http/Livewire/Admin/BoardDashboard.php
+++ app/Http/Livewire/Admin/BoardDashboard.php
@@ -4,10 +4,10 @@ namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
 
-class Dashboard extends Component
+class BoardDashboard extends Component
 {
     public function render()
     {
-        return view('livewire.admin.dashboard');
+        return view('livewire.admin.board-dashboard');
     }
 }
