4fc2c99fffb15ce3c6e50da14ccd03bd336b729a
Brannan Atkinson <brannanatkinson@gmail.com>
2022-09-09 16:28:16 -0500

    Updated result.blade to remove $1K

--- resources/views/livewire/results.blade.php
+++ resources/views/livewire/results.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="max-w-6xl px-6 md:px-0 mx-auto">
         <p class="text-center text-xl mb-2">Thank you for helping us raise</p>
-        <div class="mb-2 text-6xl text-center font-bold text-mp-blue-green">${{ number_format( App\Models\Gift::all()->sum('gift_total') + App\Models\Sponsor::all()->sum('amount') + 1000 + env('DONATIONS')  , 0, ',' ) }}</div>
+        <div class="mb-2 text-6xl text-center font-bold text-mp-blue-green">${{ number_format( App\Models\Gift::all()->sum('gift_total') + App\Models\Sponsor::all()->sum('amount') + env('DONATIONS')  , 0, ',' ) }}</div>
         <p class="text-center text-xl mb-8">to support the survivors of interpersonal violence</p>
         <div class="grid md:grid-cols-4 gap-8 mb-8">
             <div class="flex flex-col p-8 text-center text-white bg-mp-coral rounded-lg shadow-md">
