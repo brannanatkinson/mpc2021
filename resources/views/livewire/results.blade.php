2b495237ade3fd73e9efcd19d7097a5c7f94d397
Brannan Atkinson <brannanatkinson@gmail.com>
2022-09-19 10:20:48 -0500

    fixed donor from env

--- resources/views/livewire/results.blade.php
+++ resources/views/livewire/results.blade.php
@@ -6,12 +6,12 @@
         <div class="grid md:grid-cols-4 gap-8 mb-8">
             <div class="flex flex-col p-8 text-center text-white bg-mp-coral rounded-lg shadow-md">
                 <div class="mb-6 font-display"><i class="fa fa-star fa-2x"></i></div>
-                <div class="mb-6 text-5xl font-bold">{{ App\Models\Gift::all()->count() }}</div>
+                <div class="mb-6 text-5xl font-bold">{{ App\Models\Gift::all()->count() + env('DONORS') }}</div>
                 <div class="mb-4 text-xl uppercase">Donors</div>
             </div>
             <div class="flex flex-col p-8 text-center text-white bg-mp-light-lime rounded-lg shadow-md">
                 <div class="mb-6 font-display"><i class="fa fa-gift fa-2x"></i></div>
-                <div class="mb-6 text-5xl font-bold">{{ number_format(DB::table('gift_item')->select(DB::raw('SUM(item_quantity) as quantity'))->first()->quantity + env('DONORS'), 0 , ',') }}</div>
+                <div class="mb-6 text-5xl font-bold">{{ number_format(DB::table('gift_item')->select(DB::raw('SUM(item_quantity) as quantity'))->first()->quantity), 0 , ',') }}</div>
                 <div class="mb-4 text-xl uppercase">Items Given</div>
             </div>
             <div class="flex flex-col p-8 text-center text-white bg-mp-blue-green rounded-lg shadow-md">
